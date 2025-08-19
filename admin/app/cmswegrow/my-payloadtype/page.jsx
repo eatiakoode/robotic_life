32a6c08cd9cecd420aa73facbf96a504f4d0d312
Sourabh Kushwaha <sourabh@akoode.in>
2025-08-19 15:16:24 +0530

    Listing

---
+++ admin/app/cmswegrow/my-payloadtype/page.jsx
@@ -0,0 +1,10 @@
+// import MyCountryClient from "./MyCountryClient";
+import MyPayloadType from "./MyPayloadType";
+export const metadata = {
+  title: "My Payload Types || RoboticLife",
+  description: "RoboticLife - Manage your payload types",
+};
+
+export default function MyPayloadTypePage() {
+  return <MyPayloadType />;
+}
