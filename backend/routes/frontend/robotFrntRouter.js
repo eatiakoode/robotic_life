9dc0003c07d0c27768528e7ae8742f52abb67752
Aditi Sharma <aditisharma290503@gmail.com>
2025-08-27 18:51:54 +0530

    filter robots api created

--- backend/routes/frontend/robotFrntRouter.js
+++ backend/routes/frontend/robotFrntRouter.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { getRecentRobots, getallRobots } = require("../../controller/frontend/robotFrntCtrl");
+const { getRecentRobots, getallRobots, filterRobots } = require("../../controller/frontend/robotFrntCtrl");
 
 router.get("/recent", getRecentRobots);
 router.get("/all", getallRobots);
-
+router.get("/filter", filterRobots);
 module.exports = router;
